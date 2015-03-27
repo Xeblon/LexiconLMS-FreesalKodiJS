f264672df1db8c2ee767b9a17ccf89412427a3d4
RadioMalibu <brian.koudijs@icloud.com>
2015-03-27 13:52:46 +0100

    Nu har jag lagt till viewsen till controllern.

--- LexiconLMS/LexiconLMS/Controllers/LMSController.cs
+++ LexiconLMS/LexiconLMS/Controllers/LMSController.cs
@@ -14,5 +14,53 @@ namespace LexiconLMS.Controllers
         {
             return View();
         }
+
+        // COURSE HOME PAGE
+        public ActionResult Course()
+        {
+            return View();
+        }
+
+        // STUDENT PROFILE PAGE
+        public ActionResult Student()
+        {
+            return View();
+        }
+
+        // LISTING OF ALL COURSES PER ADMIN PAGE
+        public ActionResult Courses()
+        {
+            return View();
+        }
+
+        // ADD USER OR COURSE
+        public ActionResult AdminPage()
+        {
+            return View();
+        }
+
+        // THE HAND IN PAGE ON ADMIN SIDE
+        public ActionResult Handedin()
+        {
+            return View();
+        }
+
+        // THE ASSIGNMENT PAGE STUDENT SIDE
+        public ActionResult Assignments()
+        {
+            return View();
+        }
+
+        // THE SCHEDUAL PAGE
+        public ActionResult Schedual()
+        {
+            return View();
+        }
+
+        // THE SHARED FILES PAGE
+        public ActionResult SharedFiles()
+        {
+            return View();
+        }
     }
 }
