025a2236ac031bcf41e8333a2c1a88e03c9587b7
RadioMalibu <brian.koudijs@icloud.com>
2015-03-27 13:42:33 +0100

    Jag la till några views som kommer att funka som placeholder till senare. Jag har inte updaterat controllern ännu, så viewsen kan ännu inte öppnas.

---
+++ LexiconLMS/LexiconLMS/Controllers/LMSController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LexiconLMS.Controllers
+{
+    public class LMSController : Controller
+    {
+        // [Authorize(Roles = "")]
+        // GET: LMS
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
