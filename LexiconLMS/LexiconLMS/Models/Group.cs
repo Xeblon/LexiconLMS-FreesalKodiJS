b435f808912bd2834068f7aa2b5e5f551d2540f3
Xeblon <Xeblon@gmail.com>
2015-04-01 14:05:51 +0200

    Models added

---
+++ LexiconLMS/LexiconLMS/Models/Group.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace LexiconLMS.Models
+{
+    public class Group
+    {
+        [Key]
+        public int Id { get; set; }
+        public string GroupName { get; set; }
+        public int ScheduleId { get; set; }
+        [ForeignKey("ScheduleId")]
+        public virtual Schedule schedules { get; set; }
+
+        public virtual ICollection<ApplicationUser> Users { get; set; }
+
+    }
+}
---
+++ LexiconLMS/LexiconLMS/Models/Schedule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LexiconLMS.Models
+{
+    public class Schedule
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Group> Groups { get; set; }
+    }
+}
