9383f9d7d588d52fa7bad26bc17bb7f73b347383
RadioMalibu <brian.koudijs@icloud.com>
2015-04-17 17:25:49 +0200

    schedule part done

--- LexiconLMS/LexiconLMS/Controllers/HomeController.cs
+++ LexiconLMS/LexiconLMS/Controllers/HomeController.cs
@@ -1,16 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using LexiconLMS.Models;
+using LexiconLMS.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 
 namespace LexiconLMS.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
-        
+        private ApplicationDbContext db = new ApplicationDbContext();
+        List<GroupUsersViewModels> userGroupVM = new List<GroupUsersViewModels>();
+
         public ActionResult Index()
-        {
+        {   
+            var groups = db.Users.Include(s => s.Groups);
+            var userId = User.Identity.GetUserId();
+           
+            var userInfo = db.Users
+                .Where(uid => uid.Id == userId)
+                .Select(t => t.Groups)
+                .ToList();
+            var x = "";
+            foreach (var item in userInfo)
+            {
+                foreach (var group in item)
+                {
+                    x = group.ScheduleId.ToString();
+                }
+                
+            }
+
+            ViewBag.uId = x;
             return View();
         }
 
@@ -32,6 +61,7 @@ namespace LexiconLMS.Controllers
         {
             return View();
         }
+
 
     }
 }
