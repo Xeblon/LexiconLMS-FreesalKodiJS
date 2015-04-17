df0991b727542248439a51d7b9cb1d5d2de207a6
Xeblon <Xeblon@gmail.com>
2015-04-17 14:48:16 +0200

    Its HAPPENING

--- LexiconLMS/LexiconLMS/Migrations/201504161313331_Initial.Designer.cs
+++ LexiconLMS/LexiconLMS/Migrations/201504161313331_Initial.Designer.cs
@@ -7,13 +7,13 @@ namespace LexiconLMS.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.1-30610")]
-    public sealed partial class initial : IMigrationMetadata
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201504080949556_initial"; }
+            get { return "201504161313331_Initial"; }
         }
         
         string IMigrationMetadata.Source
